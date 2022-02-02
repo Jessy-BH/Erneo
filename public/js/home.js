ff73ef7fd06ae34df870a1dc77d8cab2b59c546e
Jessy-BH <berthaultjessy@gmail.com>
2022-02-02 11:46:58 +0100

    Comit public folder

---
+++ public/js/home.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+    $('.customer-logos').slick({
+        slidesToShow: 5,
+        swipeToSlide: true,
+        autoplay: true,
+        autoplaySpeed: 800,
+        arrows: false,
+        dots: false,
+            pauseOnHover: false,
+            responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 3
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 2
+            }
+        }]
+    });
+});
+
+$(document).ready(function(){
+    $('.institutional-partners').slick({
+        slidesToShow: 4,
+        swipeToSlide: true,
+        autoplay: true,
+        autoplaySpeed: 800,
+        arrows: false,
+        dots: false,
+            pauseOnHover: false,
+            responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 3
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 2
+            }
+        }]
+    });
+});
